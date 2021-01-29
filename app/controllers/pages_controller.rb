9e8feb8c187691679bd245e58b7c9343f3ae0182
Jon Norton <jmichaeln5@gmail.com>
2021-01-29 14:38:18 -0500

    Added human readable date to check in partial when nothing selected

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,13 +14,19 @@ class PagesController < ApplicationController
     @valid_clients = []
     @clients.map {|c| @valid_clients << c if c.address.present? }
     @projects = Project.all.where(user_id: current_user).order("created_at DESC")
+
   end
 
   def about
   end
 
   def test_page
-    @clients = current_user.clients.all.order("created_at DESC")
+    if current_user.present? && current_user.email == "sampleaccount@gmail.com"
+      @clients = current_user.clients.all.order(:name)
+      @projects = Project.all.where(user_id: current_user).order("created_at DESC")
+    else
+      redirect_to dashboard_path  
+    end
   end
 
   private
